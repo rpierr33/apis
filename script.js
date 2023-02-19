32cc30d97ed487b85cc96a98e81fde68d64bff19
mrralphpierre <mrralphpierre@gmail.com>
2023-02-19 10:01:34 -0500

    finished json apitraining

--- script.js
+++ script.js
@@ -7,26 +7,47 @@ function getUsers(event){
 // console.log("I am getting clicked")
     // prevent default button behavior
     event.preventDefault();
-    // // create an instance of XHR method
+    // create an instance of XHR method
     // const xhr = new XMLHttpRequest();
-    // // use open method to make our call
+    // use open method to make our call
     // xhr.open('GET', 'users.json', true)
 
     // xhr.onload = function(){
     //     if(this.status === 200){
-    //         console.log(this.responseText)
+    //         const users = JSON.parse(this.responseText)
+    //         // console.log(users)
+    //         let output = "";
+    //         users.forEach((user) => {
+    //          output += `
+    //                 <hr>
+    //                 <ul>
+    //                     <li>${user.id}</li>
+    //                     <li>${user.name}</li>
+    //                     <li>${user.age}</li>
+    //                     <li>${user.email}</li>
+    //                 </ul>
+    //          `
+    //         })
+    //         document.querySelector("#users").innerHTML = output;
     //     }
     // }
     // xhr.send();
 
     fetch("./users.json")
-    
-    .then((response) => response.json())
-    .then((data) => {
-            console.log(data)
-    })
-
-   
-
-
+      .then((response) => response.json())
+      .then((data) => {
+                let output = "";
+        data.forEach((user) => {
+          output += `
+                    <hr>
+                    <ul>
+                        <li>ID: ${user.id}</li>
+                        <li>Name: ${user.name}</li>
+                        <li>Age: ${user.age}</li>
+                        <li>Email: ${user.email}</li>
+                    </ul>
+                `;
+        });
+        document.getElementById("users").innerHTML = output;
+      });
 }
